643f8ee5c1ff3ace5de02c04760ef90f97bc82a4
Toma Andrei <toma.m.andrei@gmail.com>
2017-11-21 15:59:11 +0200

    styles added

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from "../selectors/expenses";
 import getTotalExpenses from "../selectors/expenses-total";
 import numeral from 'numeral';
@@ -8,16 +9,18 @@ export class ExpensesSummay extends React.Component{
   expenseCount = () => this.props.expenses.length;
   expensesTotal = () => numeral(getTotalExpenses(this.props.expenses)/ 100).format('$0,0.00');
   render(){
-    if(this.expenseCount()>0){
       return (
-        <div>
-          {`Viewing ${this.expenseCount()} ${this.expenseCount()>1?"expenses":"expense"} totalling ${this.expensesTotal()}`}
+        <div className="page-header">
+          <div className = "content-container">
+            <h1 className = "page-header__title">
+              Viewing <span>{this.expenseCount()}</span> {this.expenseCount() > 1 ? "expenses" : "expense"} totalling <span>{this.expensesTotal()}</span>
+            </h1>
+            <div className="page-header__actions">
+              <Link className="button" to = "/create">Add Expense</Link>
+            </div>
+          </div>
         </div>
-      );
-    }else{
-      return null;
-    }
-    
+      );  
   }
 }
 const mapStateToProps = (state) =>({ 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -18,7 +18,7 @@ const AppRouter = () => (
         <PublicRoute path="/" component = { LoginPage } exact = { true } />
         <PrivateRoute path="/dashboard" component = { ExpenseDashboardPage } />
         <PrivateRoute path="/create" component = { AddExpensePage } />
-        <PrivateRoute path="/edit/:id" component = { EditExpensePage } />
+        <PrivateRoute path="/edit/:id" component={ EditExpensePage } />
         <Route component = { NotFoundPage } />
       </Switch>
     </div>
